81aa00adcce6f074f9cb5dad3b087af692806e7c
Ben Perrin <60774052+ben2420@users.noreply.github.com>
2020-03-03 18:50:51 +0000

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,21 @@
+//check off todo's by click
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation()
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabs new todo text from input field
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
